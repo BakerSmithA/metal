433dcfde8b33e7611f9436e7e06da37bf9d6a29a
Albie Baker Smith <alexanderbakersmith@Albies-MacBook-Pro.local>
2017-05-30 14:44:59 +0100

    Done denotational semantics for statements, but not tested

--- src/Semantics/Denotational.hs
+++ src/Semantics/Denotational.hs
@@ -60,15 +60,13 @@ write s (tape, pos, envf) = Inter (tape', pos, envf) where
 
 -- Conditionally chooses to 'execute' `g1` to `g2` based on whether `p`
 -- evaulautes to True or False, respectively.
-cond :: (Config -> Bool) -> (State -> State) -> (State -> State) -> (State -> State)
-cond p b1 b2 st = do
-    c <- st
-    if p c then b1 st else b2 st
+cond :: (Config -> Bool) -> (Config -> State) -> (Config -> State) -> (Config -> State)
+cond p b1 b2 c = if p c then b1 c else b2 c
 
 -- Evaulates a while loop with condition `b` and body `stm`.
-while :: Bexp -> Stm -> State -> State
+while :: Bexp -> Stm -> Config -> State
 while b stm = fix f where
-    f g = cond (bexpVal b) (g . (stmVal stm)) id
+    f g = cond (bexpVal b) (\c -> stmVal stm c >>= g) return
 
 -- Evaulates a function declaration by adding the function to the environment.
 declFunc :: FuncName -> Stm -> Config -> State
@@ -79,18 +77,33 @@ declFunc fName stm (tape, pos, envf) = Inter (tape, pos, envf') where
 -- the function exits, the function environment is returned to what it was
 -- before the function call.
 callFunc :: FuncName -> Config -> State
-callFunc fName (tape, pos, envf) = undefined
+callFunc fName c@(tape, pos, envf) = do
+    let body          = envf fName
+    (tape', pos', _) <- stmVal body c
+    return (tape', pos', envf)
+
+-- Evaulates a command to print the symbol under the read/write head.
+-- TODO
+printRead :: Config -> State
+printRead = return
+
+-- Evaulates a command to print the supplied string.
+-- TODO
+printStr :: String -> Config -> State
+printStr s = return
 
 -- The semantic function S[[.]] over statements. The writer returned keeps
 -- track of the text output of the statement.
-stmVal :: Stm -> State -> State
-stmVal (MoveLeft)           st = st >>= moveLeft
-stmVal (MoveRight)          st = st >>= moveRight
-stmVal (Write s)            st = st >>= write s
-stmVal (Reject)             st = HaltR
-stmVal (Accept)             st = HaltA
-stmVal (If b stm)           st = (cond (bexpVal b) (stmVal stm) id) st
-stmVal (IfElse b stm1 stm2) st = (cond (bexpVal b) (stmVal stm1) (stmVal stm2)) st
-stmVal (While b stm)        st = while b stm st
-stmVal (Func fName stm)     st = st >>= (declFunc fName stm)
-stmVal (Call fName)         st = undefined
+stmVal :: Stm -> Config -> State
+stmVal (MoveLeft)           = moveLeft
+stmVal (MoveRight)          = moveRight
+stmVal (Reject)             = \c -> HaltR
+stmVal (Accept)             = \c -> HaltA
+stmVal (If b stm)           = cond (bexpVal b) (stmVal stm) return
+stmVal (IfElse b stm1 stm2) = cond (bexpVal b) (stmVal stm1) (stmVal stm2)
+stmVal (While b stm)        = while b stm
+stmVal (Func fName body)    = declFunc fName body
+stmVal (Call fName)         = callFunc fName
+stmVal (Comp stm1 stm2)     = \c -> (stmVal stm1 c) >>= (stmVal stm2)
+stmVal (PrintRead)          = printRead
+stmVal (PrintStr str)       = printStr str
