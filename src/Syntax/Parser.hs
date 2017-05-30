b4c194ab1c1ceed69575eddf66fc43b250fc3d53
Albie Baker Smith <alexanderbakersmith@Albies-MacBook-Pro.local>
2017-05-30 01:46:57 +0100

    Added parser for statements, and tested the parser

--- src/Syntax/Parser.hs
+++ src/Syntax/Parser.hs
@@ -51,13 +51,22 @@ whitespace = L.space (void spaceChar) lineCmnt blockCmnt
   where lineCmnt  = L.skipLineComment "//"
         blockCmnt = L.skipBlockComment "/*" "*/"
 
+-- Succeeds if the specified string can be parsed, followed by any ignored
+-- whitespace.
+tok :: String -> Parser String
+tok s = string s <* whitespace
+
 -- Parses a string enclosed in parenthesis.
 parens :: Parser a -> Parser a
-parens = between (string "(") (string ")")
+parens = between (tok "(") (tok ")")
+
+-- Parses a string enclosed in curly braces.
+braces :: Parser a -> Parser a
+braces = between (tok "{") (tok "}")
 
 -- Parses a string encased in double quotes.
 encasedString :: Parser String
-encasedString = between (string "\"") (string "\"") (many alphaNumChar)
+encasedString = between (tok "\"") (tok "\"") (many (noneOf "\""))
 
 -- The type representing a tape symbol, i.e. a symbol contained in a cell of
 -- the machine's tape.
@@ -66,20 +75,22 @@ type TapeSymbol = Char
 -- Parses a tape symbol, the EBNF syntax of which is:
 --  TapeSymbol  : LowerChar | UpperChar | Digit | '+' | '/'
 tapeSymbol :: Parser TapeSymbol
-tapeSymbol = alphaNumChar
-         <|> char '+'
-         <|> char '/'
+tapeSymbol = tapeSymbol' <* whitespace where
+    tapeSymbol' = alphaNumChar
+             <|> char '+'
+             <|> char '/'
 
 -- The type represented a derived symbol, i.e. either a literal tape symbol, or
 -- a symbol read from under the read/write head.
 data DerivedSymbol = Literal TapeSymbol
                    | Read
+                   deriving (Eq, Show)
 
 -- Parses a derived symbol, the EBNF syntax of which is:
 --   DerivedSymbol : 'read' | TapeSymbol
 derivedSymbol :: Parser DerivedSymbol
-derivedSymbol = Literal <$> tapeSymbol
-            <|> Read <$ string "read"
+derivedSymbol = Read <$ tok "read"
+            <|> Literal <$> tapeSymbol
 
 -- The type representing a function name.
 type FuncName = String
@@ -89,8 +100,8 @@ type FuncName = String
 -- A practical consideration when parsing function names is that they do not
 -- conflict with reserved keywords.
 funcName :: Parser FuncName
-funcName = (str >>= check) where
-    str        = (:) <$> letterChar <*> many alphaNumChar
+funcName = (str >>= check) <* whitespace where
+    str        = (:) <$> lowerChar <*> many alphaNumChar
     check word = if word `elem` reservedKeywords
                     then fail $ "keyword " ++ show word ++ " cannot be an identifier"
                     else return word
@@ -103,21 +114,22 @@ data Bexp = TRUE
           | Or Bexp Bexp
           | Eq DerivedSymbol DerivedSymbol
           | Le DerivedSymbol DerivedSymbol
+          deriving (Eq, Show)
 
 -- Parses the basis elements of the boolean expressions, plus boolean
 -- expressions wrapped in parenthesis.
 bexp' :: Parser Bexp
 bexp' = try (parens bexp)
-    <|> TRUE  <$ string "True"
-    <|> FALSE <$ string "False"
-    <|> try (Eq <$> derivedSymbol <* string " == " <*> derivedSymbol)
-    <|> try (Le <$> derivedSymbol <* string " <= " <*> derivedSymbol)
+    <|> TRUE  <$ tok "True"
+    <|> FALSE <$ tok "False"
+    <|> try (Eq <$> derivedSymbol <* tok "==" <*> derivedSymbol)
+    <|> try (Le <$> derivedSymbol <* tok "<=" <*> derivedSymbol)
 
 -- The operators that can work on boolean expressions. There is no precedence,
 -- instead the expression is evaualted from left to right.
 bexpOps :: [[Operator Parser Bexp]]
-bexpOps = [[Prefix (Not <$ string "not ")],
-           [InfixL (And <$ string " and ")]]
+bexpOps = [[Prefix (Not <$ tok "not")],
+           [InfixL (And <$ tok "and"), InfixL (Or <$ tok "or")]]
 
 -- Parses a boolean expression, allowing for parenthesis to specify the
 -- intended parse. The EBNF syntax of boolean expressions is:
@@ -132,8 +144,8 @@ bexp :: Parser Bexp
 bexp = makeExprParser bexp' bexpOps
 
 -- The type that represents the syntax tree for statements.
-data Stm = Left
-         | Right
+data Stm = MoveLeft
+         | MoveRight
          | Write TapeSymbol
          | Reject
          | Accept
@@ -144,15 +156,27 @@ data Stm = Left
          | Call FuncName
          | Comp Stm Stm
          | PrintRead
-         | PrintStr
+         | PrintStr String
+         deriving (Eq, Show)
 
 -- Parses the elements of the syntactic class Stm, except for composition.
 stm' :: Parser Stm
-stm' = undefined
+stm' = MoveLeft <$ tok "left"
+   <|> MoveRight <$ tok "right"
+   <|> Write <$ tok "write" <*> tapeSymbol
+   <|> Reject <$ tok "reject"
+   <|> Accept <$ tok "accept"
+   <|> try (IfElse <$ tok "if" <*> bexp <*> braces stm <* tok "else" <*> braces stm)
+   <|> If <$ tok "if" <*> bexp <*> braces stm
+   <|> While <$ tok "while" <*> bexp <*> braces stm
+   <|> Func <$ tok "func" <*> funcName <*> braces stm
+   <|> Call <$ tok "call" <*> funcName
+   <|> try (PrintStr <$ tok "print" <*> encasedString)
+   <|> PrintRead <$ tok "print"
 
 -- The operators that can work on statements.
 stmOps :: [[Operator Parser Stm]]
-stmOps = undefined
+stmOps = [[InfixL (Comp <$ tok ";")]]
 
 -- Parses a statement, the EBNF syntax of statements being:
 --  Stm : 'left'
@@ -168,4 +192,4 @@ stmOps = undefined
 --      | 'print'
 --      | 'print' String
 stm :: Parser Stm
-stm = undefined
+stm = makeExprParser stm' stmOps
--- test/Syntax/ParserSpec.hs
+++ test/Syntax/ParserSpec.hs
@@ -1,9 +1,243 @@
 module Syntax.ParserSpec where
 
+import Syntax.Parser
+
+import Text.Megaparsec
 import Test.Hspec
+import Test.Hspec.Megaparsec
 
 parserSpec :: Spec
 parserSpec = do
-    describe "Tests" $ do
-      it "Testing test" $ do
-       True `shouldBe` True
+    encasedStringSpec
+    tapeSymbolSpec
+    derivedSymbolSpec
+    funcNameSpec
+    bexpSpec
+    stmSpec
+
+encasedStringSpec :: Spec
+encasedStringSpec = do
+    describe "encasedString" $ do
+        it "parses the empty string" $ do
+            parse encasedString "" "\"\"" `shouldParse` ""
+
+        it "parses a string enclosed in double quotes" $ do
+            parse encasedString "" "\"str\"" `shouldParse` "str"
+
+        it "parses strings enclosed in double quotes that contain spaces" $ do
+            parse encasedString "" "\"This is a string\"" `shouldParse` "This is a string"
+
+        it "fails to parse if a starting quote is missing" $ do
+            parse encasedString "" `shouldFailOn` "str\""
+
+        it "fails to parse if an ending quote is missing" $ do
+            parse encasedString "" `shouldFailOn` "\"str"
+
+tapeSymbolSpec :: Spec
+tapeSymbolSpec = do
+    describe "tapeSymbol" $ do
+        it "parses lowercase letters" $ do
+            parse tapeSymbol "" "a" `shouldParse` 'a'
+
+        it "parses uppercase letters" $ do
+            parse tapeSymbol "" "A" `shouldParse` 'A'
+
+        it "parses digits" $ do
+            parse tapeSymbol "" "1" `shouldParse` '1'
+
+        it "parses a plus symbol" $ do
+            parse tapeSymbol "" "+" `shouldParse` '+'
+
+        it "parses a division symbol" $ do
+            parse tapeSymbol "" "/" `shouldParse` '/'
+
+        it "fails to parse various symbols" $ do
+            parse tapeSymbol "" `shouldFailOn` "="
+            parse tapeSymbol "" `shouldFailOn` "-"
+            parse tapeSymbol "" `shouldFailOn` ";"
+            parse tapeSymbol "" `shouldFailOn` "|"
+
+derivedSymbolSpec :: Spec
+derivedSymbolSpec = do
+    describe "derivedSymbol" $ do
+        it "parses literals" $ do
+            parse derivedSymbol "" "b" `shouldParse` Literal 'b'
+            parse derivedSymbol "" "B" `shouldParse` Literal 'B'
+            parse derivedSymbol "" "1" `shouldParse` Literal '1'
+
+        it "parses read" $ do
+            parse derivedSymbol "" "read" `shouldParse` Read
+
+funcNameSpec :: Spec
+funcNameSpec = do
+    describe "funcName" $ do
+        it "parses names beginning with a lower char" $ do
+            parse funcName "" "fname" `shouldParse` "fname"
+
+        it "parses names containing uppcase characters" $ do
+            parse funcName "" "fNAME" `shouldParse` "fNAME"
+
+        it "parses names containing digits" $ do
+            parse funcName "" "f1234" `shouldParse` "f1234"
+
+        it "parses if the function name is a superset of a reserved word" $ do
+            parse funcName "" "trueF" `shouldParse` "trueF"
+
+        it "fails if the start character is uppercase" $ do
+            parse funcName "" `shouldFailOn` "Fname"
+
+        it "fails if the start character is a digit" $ do
+            parse funcName "" `shouldFailOn` "1fName"
+
+        it "fails if the function name is a reserved keyword" $ do
+            parse funcName "" `shouldFailOn` "True"
+
+bexpSpec :: Spec
+bexpSpec = describe "bexp" $ do
+    context "parsing basis elements" $ do
+        it "parses True" $ do
+            parse bexp "" "True" `shouldParse` TRUE
+
+        it "parses False" $ do
+            parse bexp "" "False" `shouldParse` FALSE
+
+    context "parsing NOT operator" $ do
+        it "parses NOT" $ do
+            parse bexp "" "not False" `shouldParse` (Not FALSE)
+
+    context "parsing AND operator" $ do
+        it "parses AND" $ do
+            parse bexp "" "True and False" `shouldParse` (And TRUE FALSE)
+
+        it "parse to be left associative" $ do
+            parse bexp "" "True and False and False" `shouldParse` (And (And TRUE FALSE) FALSE)
+
+    context "parsing OR operator" $ do
+        it "parses OR" $ do
+            parse bexp "" "True or False" `shouldParse` (Or TRUE FALSE)
+
+        it "parses to be left associative" $ do
+            parse bexp "" "True or False or False" `shouldParse` (Or (Or TRUE FALSE) FALSE)
+
+    context "parsing EQ operator" $ do
+        it "parses EQ" $ do
+            parse bexp "" "x == read" `shouldParse` (Eq (Literal 'x') Read)
+
+        it "fails to parse chains" $ do
+            parse bexp "" "x == y == z" `shouldParse` (Eq (Literal 'x') (Literal 'y'))
+
+    context "parsing LE operator" $ do
+        it "parses LE" $ do
+            parse bexp "" "x <= read" `shouldParse` (Le (Literal 'x') Read)
+
+        it "fails to parse chains" $ do
+            parse bexp "" "x <= y <= z" `shouldParse` (Le (Literal 'x') (Literal 'y'))
+
+    context "parsing boolean expressions with parenthesis" $ do
+        it "gives precedence to bracketed expressions" $ do
+            parse bexp "" "True and (False or False)" `shouldParse` (And TRUE (Or FALSE FALSE))
+
+        it "allows brackets at the top level" $ do
+            parse bexp "" "(True)" `shouldParse` TRUE
+
+stmSpec :: Spec
+stmSpec = describe "stm" $ do
+    context "parsing Turing Machine operators" $ do
+        it "parses LEFT command" $ do
+            parse stm "" "left" `shouldParse` MoveLeft
+
+        it "parses RIGHT command" $ do
+            parse stm "" "right" `shouldParse` MoveRight
+
+        it "parses WRITE command" $ do
+            parse stm "" "write x" `shouldParse` (Write 'x')
+
+        it "parses REJECT" $ do
+            parse stm "" "reject" `shouldParse` Reject
+
+        it "parses ACCEPT" $ do
+            parse stm "" "accept" `shouldParse` Accept
+
+    context "parsing IF statements" $ do
+        it "parses IF" $ do
+            parse stm "" "if True { right }" `shouldParse` (If TRUE MoveRight)
+
+        it "fails to parse if a boolean expression is missing" $ do
+            parse stm "" `shouldFailOn` "if { right }"
+
+        it "fails to parse if the first brace is missing" $ do
+            parse stm "" `shouldFailOn` "if True right }"
+
+        it "fails to parse if the second brace is missing" $ do
+            parse stm "" `shouldFailOn` "if True { right"
+
+        it "fails to parse if both braces are missing" $ do
+            parse stm "" `shouldFailOn` "if True right"
+
+    context "parsing IF-ELSE statements" $ do
+        it "parses IF-ELSE" $ do
+            parse stm "" "if True { right } else { left }" `shouldParse` (IfElse TRUE MoveRight MoveLeft)
+
+        it "fails to parse if a boolean expression is missing" $ do
+            parse stm "" `shouldFailOn` "if { right } else { left }"
+
+        it "fails to parse if the first brace is missing" $ do
+            parse stm "" `shouldFailOn` "if True right } else { left }"
+
+        it "fails to parse if the second brace is missing" $ do
+            parse stm "" `shouldFailOn` "if True { right else { left }"
+
+        it "fails to parse if the third brace is missing" $ do
+            parse stm "" `shouldFailOn` "if { right } else left }"
+
+        it "fails to parse if the fourth brace is missing" $ do
+            parse stm "" `shouldFailOn` "if { right } else { left"
+
+        it "fails to parse if all braces are missing" $ do
+            parse stm "" `shouldFailOn` "if right else left"
+
+    context "parsing WHILE statements" $ do
+        it "parses WHILE" $ do
+            parse stm "" "while True { right }" `shouldParse` (While TRUE MoveRight)
+
+        it "fails to parse if a boolean expression is missing" $ do
+            parse stm "" `shouldFailOn` "while { right }"
+
+        it "fails to parse if the first brace is missing" $ do
+            parse stm "" `shouldFailOn` "while True right }"
+
+        it "fails to parse if the second brace is missing" $ do
+            parse stm "" `shouldFailOn` "while True { right"
+
+        it "fails to parse if both braces are missing" $ do
+            parse stm "" `shouldFailOn` "while True right"
+
+    context "parsing function declarations" $ do
+        it "parses function delcarations" $ do
+            parse stm "" "func fName { right }" `shouldParse` (Func "fName" MoveRight)
+
+        it "fails to parse if a function name is missing" $ do
+            parse stm "" `shouldFailOn` "func { right }"
+
+        it "fails to parse if the first brace is missing" $ do
+            parse stm "" `shouldFailOn` "func fName right }"
+
+        it "fails to parse if the second brace is missing" $ do
+            parse stm "" `shouldFailOn` "func fName { right"
+
+        it "fails to parse if both braces are missing" $ do
+            parse stm "" `shouldFailOn` "func fName right"
+
+    context "parsing function calls" $ do
+        it "parses function calls" $ do
+            parse stm "" "call fName" `shouldParse` (Call "fName")
+
+        it "fails to parse if no function name is given" $ do
+            parse stm "" `shouldFailOn` "call"
+
+    context "parsing printing" $ do
+        it "parses printing a string" $ do
+            parse stm "" "print \"This is a string\"" `shouldParse` (PrintStr "This is a string")
+
+        it "parses printing the symbol read from the tape" $ do
+            parse stm "" "print" `shouldParse` PrintRead
