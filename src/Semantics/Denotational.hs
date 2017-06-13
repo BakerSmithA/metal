a851bc6e2c6dd361aa2b42cb7f9b784bb730afef
Albie Baker Smith <alexanderbakersmith@Albies-MacBook-Pro.local>
2017-06-13 07:11:56 +0100

    Added types to represent state for denotational semantics

--- src/Semantics/Denotational.hs
+++ src/Semantics/Denotational.hs
@@ -6,12 +6,14 @@ import State.Config
 import State.Env
 import Semantics.Helpers
 import Control.Monad.Reader
+import Control.Monad.Except
 import Data.Maybe
 
-type ReaderState = ReaderT Env Machine Config
+type StateM a     = ReaderT Env Machine a
+type StateMConfig = StateM Config
 
 -- The semantic function D[[.]] over tape symbols.
-derivedSymbolVal :: DerivedSymbol -> ReaderState -> ReaderT Env Machine TapeSymbol
+derivedSymbolVal :: DerivedSymbol -> StateMConfig -> StateM TapeSymbol
 derivedSymbolVal (Read)        r = mapReaderT getCurr r
 derivedSymbolVal (Literal sym) r = return sym
 derivedSymbolVal (Var name)    r = do
@@ -20,7 +22,7 @@ derivedSymbolVal (Var name)    r = do
     return val
 
 -- The semantic function B[[.]] over boolean expressions.
-bexpVal :: Bexp -> ReaderState -> ReaderT Env Machine Bool
+bexpVal :: Bexp -> StateMConfig -> StateM Bool
 bexpVal (TRUE)      r = return True
 bexpVal (FALSE)     r = return False
 bexpVal (Not b)     r = liftM not (bexpVal b r)
@@ -32,7 +34,7 @@ bexpVal (Le s1 s2)  r = liftM2 (<=) (derivedSymbolVal s1 r) (derivedSymbolVal s2
 -- Conditionally chooses to 'execute' a branch if associated predicate
 -- evaluates to true. Returns the branch to execute, or `id` if no predicates
 -- evaluate to true.
-cond :: [(ReaderState -> ReaderT Env Machine Bool, ReaderState -> ReaderState)] -> (ReaderState -> ReaderState)
+cond :: [(StateMConfig -> StateM Bool, StateMConfig -> StateMConfig)] -> (StateMConfig -> StateMConfig)
 cond []                       r = r
 cond ((predicate, branch):ps) r = do
     bVal <- predicate r
@@ -40,73 +42,72 @@ cond ((predicate, branch):ps) r = do
             else cond ps r
 
 -- Evaluates moving the read-write head one cell to the left.
-evalLeft :: ReaderState -> ReaderState
+evalLeft :: StateMConfig -> StateMConfig
 evalLeft = mapReaderT left
 
 -- Evaluates moving the read-write head one cell to the right.
-evalRight :: ReaderState -> ReaderState
+evalRight :: StateMConfig -> StateMConfig
 evalRight = mapReaderT right
 
 -- Evaluates writing to the tape.
-evalWrite :: ReaderT Env Machine TapeSymbol -> ReaderState -> ReaderState
+evalWrite :: StateM TapeSymbol -> StateMConfig -> StateMConfig
 evalWrite sym r = do
     val <- sym
     mapReaderT (setCurr val) r
 
 -- Evaluates halting the machine by accepting.
-evalAccept :: ReaderState
+evalAccept :: StateMConfig
 evalAccept = lift HaltA
 
 -- Evaluates halting the machine by rejecting.
-evalReject :: ReaderState
+evalReject :: StateMConfig
 evalReject = lift HaltR
 
 -- Evaluates an if-else statement.
-evalIf :: Bexp -> Stm -> [(Bexp, Stm)] -> Maybe Stm -> ReaderState -> ReaderState
+evalIf :: Bexp -> Stm -> [(Bexp, Stm)] -> Maybe Stm -> StateMConfig -> StateMConfig
 evalIf b stm elseIfClauses elseStm = cond branches where
     branches   = map (\(b, stm) -> (bexpVal b, evalStm stm)) allClauses
     allClauses = ((b, stm):elseIfClauses) ++ (maybeToList elseClause)
     elseClause = fmap (\stm -> (TRUE, stm)) elseStm
 
 -- Evaluates a while loop.
-evalWhile :: Bexp -> Stm -> ReaderState -> ReaderState
+evalWhile :: Bexp -> Stm -> StateMConfig -> StateMConfig
 evalWhile b stm = fix f where
-    f :: (ReaderState -> ReaderState) -> ReaderState -> ReaderState
     f loop = cond [(bexpVal b, (evalStm stm) . loop)]
 
 -- Evaluates a variable declaration.
-evalVarDecl :: VarName -> DerivedSymbol -> ReaderState -> ReaderState
+evalVarDecl :: VarName -> DerivedSymbol -> StateMConfig -> StateMConfig
 evalVarDecl name sym r = do
     val <- derivedSymbolVal sym r
     local (addVar name val) r
 
 -- Evaluates a function declaration.
-evalFuncDecl :: FuncName -> Stm -> ReaderState -> ReaderState
+evalFuncDecl :: FuncName -> Stm -> StateMConfig -> StateMConfig
 evalFuncDecl name body = local (addFunc name body)
 
 -- Evaluates a function call.
-evalCall :: FuncName -> ReaderState -> ReaderState
+evalCall :: FuncName -> StateMConfig -> StateMConfig
 evalCall name r = do
     -- TODO: Error handling.
     Just body <- asks (lookupFunc name)
     evalStm body r
 
 -- Evaluates the composition of two statements.
-evalComp :: Stm -> Stm -> ReaderState -> ReaderState
+evalComp :: Stm -> Stm -> StateMConfig -> StateMConfig
 evalComp stm1 stm2 = (evalStm stm1) . (evalStm stm2)
 
 -- Evaluates print the symbol under the read-write head.
 -- TODO: Add I/O.
-evalPrintRead :: ReaderState -> ReaderState
+evalPrintRead :: StateMConfig -> StateMConfig
 evalPrintRead = id
 
 -- Evaluates string an arbitrary string.
 -- TODO: Add I/O.
-evalPrintStr :: String -> ReaderState -> ReaderState
+evalPrintStr :: String -> StateMConfig -> StateMConfig
 evalPrintStr str = id
 
 -- Evalautes a statement in a configuration of a Turing machine.
-evalStm :: Stm -> ReaderState -> ReaderState
+evalStm :: Stm -> StateMConfig -> StateMConfig
 evalStm (MoveLeft)                = evalLeft
 evalStm (MoveRight)               = evalRight
 evalStm (Write sym)               = both evalWrite (derivedSymbolVal sym)
