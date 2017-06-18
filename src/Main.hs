2be0e2e190898a8c5f835616ba52fea5c22caeea
Albie Baker Smith <bakersmitha@gmail.com>
2017-06-18 22:00:29 +0100

    Denotational semantics implemented using monad stack. Not yet tested

--- src/Main.hs
+++ src/Main.hs
@@ -2,8 +2,26 @@ module Main where
 
 import Syntax.Tree
 import Semantics.Denotational
+import System.Environment
+import Text.Megaparsec
+import Text.Megaparsec.String
+
+-- There are two stages in which errors could occur: parsing and
+-- during runtime (semantic).
+data Error a b = ParseError a    -- An error whilst parsing code.
+               | SemanticError b -- An error whilst running the program.
+
+
+-- type Prog a     = ReaderT Env (ExceptT RuntimeError (MachineT IO)) a
+
+-- Attempts to parse the specified file.
+parseFromFile :: Parser a -> FilePath -> IO (Either (ParseError Char Dec) a)
+parseFromFile p file = runParser p file <$> readFile file
+
+runProg :: Stm -> IO Config
+runProg = undefined
 
 main :: IO ()
 main = do
-    let stm = (Comp (PrintStr "STR") Accept)
-    return ()
+    [sourceName, str] <- getArgs
+    parsed            <- parseFromFile stm sourceName
