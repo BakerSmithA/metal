0dd7e77056a9b5c4debe85c3dd9f77f7a30875bd
Albie Baker Smith <bakersmitha@gmail.com>
2017-06-22 11:33:53 +0100

    Added tests for getSym in Tape

--- test/State/TapeSpec.hs
+++ test/State/TapeSpec.hs
@@ -1,11 +1,18 @@
 module State.TapeSpec (tapeSpec) where
 
 import State.Tape
+import Syntax.Tree
+import TestHelper
 import Test.Hspec
+
+-- Asserts that the tape should contain `syms` at its start.
+shouldRead :: Tape -> [TapeSymbol] -> Expectation
+shouldRead tape syms = tapeShouldRead tape syms `shouldBe` True
 
 tapeSpec :: Spec
 tapeSpec = do
     fromStringSpec
+    getSymSpec
     setSymSpec
 
 fromStringSpec :: Spec
@@ -13,13 +20,26 @@ fromStringSpec = do
     describe "fromString" $ do
         it "places a string at the start of the tape" $ do
             let tape = fromString "abc"
-            getSym 0 tape `shouldBe` 'a'
-            getSym 1 tape `shouldBe` 'b'
-            getSym 2 tape `shouldBe` 'c'
+            tape `shouldRead` "abc"
 
         it "leaves the rest of the tape as space characters" $ do
             let tape = fromString "abc"
             getSym 3 tape `shouldBe` ' '
+
+getSymSpec :: Spec
+getSymSpec = do
+    describe "getSym" $ do
+        it "returns the symbol at the specified position on the tape" $ do
+            let tape = fromString "abc"
+            getSym 0 tape `shouldBe` 'a'
+            getSym 1 tape `shouldBe` 'b'
+            getSym 2 tape `shouldBe` 'c'
+
+        it "returns the blank (space) character if the cell is empty" $ do
+            let tape = fromString "abc"
+            getSym 3 tape `shouldBe` ' '
+            getSym 4 tape `shouldBe` ' '
+            getSym 5 tape `shouldBe` ' '
 
 setSymSpec :: Spec
 setSymSpec = do
@@ -27,12 +47,9 @@ setSymSpec = do
         it "sets the tape symbol" $ do
             let tape  = fromString "abc"
                 tape' = setSym 1 'x' tape
-            getSym 1 tape' `shouldBe` 'x'
+            tape' `shouldRead` "axc"
 
         it "does not set symbol other than that specified" $ do
             let tape = fromString "abc"
                 tape' = setSym 1 'x' tape
-
-            getSym 0 tape' `shouldBe` 'a'
-            getSym 2 tape' `shouldBe` 'c'
-            getSym 3 tape' `shouldBe` ' '
+            tape' `shouldRead` "axc"
--- test/TestHelper.hs
+++ test/TestHelper.hs
@@ -7,7 +7,7 @@ import Syntax.Tree
 import Test.Hspec
 import Test.HUnit.Lang
 
--- Asserts that the tape should contain the string `str` at its start.
+-- Returns whether the tape should contain the string `syms` at its start.
 tapeShouldRead :: Tape -> [TapeSymbol] -> Bool
 tapeShouldRead tape syms = tape == expected where
     expected = fromList (zip [0..] syms)
