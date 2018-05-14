22a6a73c4bf45b31aabcd8f6ac3bb09afebb558d
Albie Baker-Smith <bakersmitha@gmail.com>
2018-05-14 14:21:18 +0100

    Import uses monad

--- src/Semantics/Program.hs
+++ src/Semantics/Program.hs
@@ -8,14 +8,18 @@ import State.Output
 
 -- Describes a path in the tree, this could represent a file system path.
 type TreePath = String
+type Tree m = (TreePath -> m ([TreePath], Stm))
 
--- Perfoms a depth first search resolving imports into the statement in those
--- files. `tree` describes the shape of the tree by giving a list of branches
+-- Perfoms a DF search resolving imports into the statement in those files.
+-- `tree` describes the shape of the tree by giving a list of branches
 -- (import statements) in that file.
-importStms :: (TreePath -> ([TreePath], Stm)) -> TreePath -> [Stm]
-importStms tree initialPath = foldr f [body] imports where
-    f path acc = (importStms tree path) ++ acc
-    (imports, body) = tree initialPath
+importStms :: (Monad m) => Tree m -> [TreePath] -> m [Stm]
+importStms _ [] = return []
+importStms tree (path:rest) = do
+    (imports, stm) <- tree path
+    childrenStms <- importStms tree imports
+    restStms <- importStms tree rest
+    return (childrenStms ++ [stm] ++ restStms)
 
 -- Evalutes the program, and defaults to accepting if no terminating state is
 -- reached.
--- test/Semantics/ProgramSpec.hs
+++ test/Semantics/ProgramSpec.hs
@@ -1,5 +1,6 @@
 module Semantics.ProgramSpec (programSpec) where
 
+import Control.Monad.Identity
 import Semantics.Program
 import Syntax.Tree
 import State.Config as Config
@@ -8,11 +9,11 @@ import Test.Hspec
 
 -- Simulates a file structure, returning the contents of a file given the file
 -- path.
-testTree :: TreePath -> ([TreePath], Stm)
-testTree "File1" = (["File2", "File3"], PrintStr "1")
-testTree "File2" = (["File4"], PrintStr "2")
-testTree "File3" = ([], PrintStr "3")
-testTree "File4" = ([], PrintStr "4")
+testTree :: TreePath -> Identity ([TreePath], Stm)
+testTree "File1" = return (["File2", "File3"], PrintStr "1")
+testTree "File2" = return (["File4"], PrintStr "2")
+testTree "File3" = return ([], PrintStr "3")
+testTree "File4" = return ([], PrintStr "4")
 testTree _       = error "No file"
 
 programSpec :: Spec
@@ -27,7 +28,7 @@ importStmsSpec = do
             -- Resolving the dependencies we get the order of imports to be:
             -- File4, File2, File3, File1.
             let expected = [PrintStr "4", PrintStr "2", PrintStr "3", PrintStr "1"]
-            importStms testTree "File1" `shouldBe` expected
+            importStms testTree ["File1"] `shouldBe` (return expected)
 
         it "returns nothing if there is a cycle in the dependencies" $ do
             pending
